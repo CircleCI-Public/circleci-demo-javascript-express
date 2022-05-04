59c5ce829f20d4e71d674869216778d229fef99a
jaindeepak2008 <jaindeepak2008@gmail.com>
2022-05-03 21:50:20 -0400

    small change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
   // In production, serve the webpacked server file.
   require('./dist/server.bundle.js');
 } else {
-  // Babel polyfill to convert ES6 code in runtime
+  // Babel polyfill to convert ES6 code in runtime.
   require('babel-register')({
     "plugins": [
       [
